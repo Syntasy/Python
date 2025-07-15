71ca3f29917bfbea201eaae0aa8fbb3710df52fc
Syntasy <schullarj@proton.me>
2025-07-15 13:09:43 -0400

    broken

--- readwrite/readwrite.py
+++ readwrite/readwrite.py
@@ -1,4 +1,18 @@
 #!/bin/python3
 
-months = open('months.txt')
-print(months.read())
+# months = open('months.txt')
+
+
+# for month in months:
+#     print(month.strip())
+# print(months.readlines())
+# months.seek(0)
+# print(months.readlines())
+# print(months.read())
+
+# months.close()
+
+days = open('days.txt', "a")
+days.write("\nwednesday")
+
+days.close()
